ad1ac606ef018ef0578f782a7805974f36624f01
Tim Kwak <tim.k.kwak@gmail.com>
2017-12-30 10:41:04 -0500

    Add Session API Util

--- frontend/disclone.jsx
+++ frontend/disclone.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import * as sessionAPIUtil from './util/session_api_util';
+
+window.sessionAPIUtil = sessionAPIUtil;
+
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
   ReactDOM.render(<h1>Working</h1>, root);
