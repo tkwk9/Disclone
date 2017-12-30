72722311578a9dcecaf87271f884cc0c81590310
Tim Kwak <tim.k.kwak@gmail.com>
2017-12-29 21:19:34 -0500

    Add backend auth

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,9 +1,27 @@
 class Api::SessionsController < ApplicationController
 
   def create
+    @user = User.find_by_credentials(user_params[:email], user_params[:password])
+    if @user
+      login(@user)
+      render 'api/users/show.json.jbuilder'
+    else
+      render json: ["Invalid credentials"], status: 403
+    end
   end
 
   def destroy
+    if current_user
+      logout
+      render json: {}
+    else
+      render json: ["User not logged in"], status: 403
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:email, :password)
   end
 
 end
--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,9 +1,23 @@
 class Api::UsersController < ApplicationController
 
   def index
+    @users = User.all
+    render :index
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      login(@user)
+      render :show
+    else
+      render json: @user.errors.full_messages, status: 403
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:email, :username, :password)
   end
 
 end
