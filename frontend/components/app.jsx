dbe5e5a98df921145f787aa9c0ce41fa5220d391
Tim Kwak <tim.k.kwak@gmail.com>
2017-12-30 13:01:35 -0500

    Setup SessionForm

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import { Switch, Route } from 'react-router';
+import SessionForm from './session/session_form';
 
 export default () => {
   return (
-    <div>
-      <h1>Discord</h1>
+    <div className='app'>
+      <Switch>
+        <Route path='/login' component={SessionForm} />
+        <Route path='/signup' component={SessionForm} />
+      </Switch>
+      <div className='main-page'>
+        <h1>Mainpage</h1>
+      </div>
     </div>
   );
 };
