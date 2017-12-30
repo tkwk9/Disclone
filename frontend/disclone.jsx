12464520245cfcfb3ed32a26be51e3ac24d49fb2
Tim Kwak <tim.k.kwak@gmail.com>
2017-12-30 12:11:02 -0500

    Setup basic components

--- frontend/disclone.jsx
+++ frontend/disclone.jsx
@@ -1,8 +1,7 @@
-
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
+import Root from './components/root';
 
 // ### TESTING ###
 import * as sessionAPIUtil from './util/session_api_util';
@@ -13,8 +12,11 @@ window.sessionActions = sessionActions;
 
 document.addEventListener('DOMContentLoaded', () => {
   const store = configureStore();
+
+  // ### TESTING ###
   window.store = store;
+  // ### TESTING ###
 
   const root = document.getElementById('root');
-  ReactDOM.render(<h1>Working</h1>, root);
+  ReactDOM.render(<Root store={ store }/>, root);
 });
