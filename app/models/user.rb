259b4cfe392d05300c11b15dd9368bb05c25c4a3
Tim Kwak <tim.k.kwak@gmail.com>
2017-12-29 20:58:36 -0500

    Add user

---
+++ app/models/user.rb
@@ -0,0 +1,56 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :integer          not null, primary key
+#  email           :string           not null
+#  username        :string           not null
+#  password_digest :string           not null
+#  session_token   :string           not null
+#  img_url         :string
+#  online          :boolean          not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+
+class User < ApplicationRecord
+
+  validates :email, :username, :password_digest, :session_token, presence: true
+  validates :email, uniqueness: true
+  validates :online, inclusion: { in: [true, false] }
+  validates :password, length: {minimum: 6, allow_nil: true}
+
+  after_initialize :ensure_session_token
+
+  attr_reader :password
+
+  def self.find_by_credentials(email, password)
+    user = User.find_by(email: email)
+    if user && BCrypt::Password.new(user.password_digest).is_password?(password)
+      return user
+    else
+      nil
+    end
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def init
+    self.session_token ||= SecureRandom.urlsafe_base64
+    self.online ||= false;
+  end
+
+  def ensure_session_token
+    self.session_token ||= SecureRandom.urlsafe_base64
+  end
+
+  def reset_session_token!
+    self.session_token = SecureRandom.urlsafe_base64
+    save!
+    self.session_token
+  end
+
+end
