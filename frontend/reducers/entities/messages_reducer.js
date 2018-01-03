d6a53352ed6479623e6154e7dbd9e48132d0f0f0
Tim Kwak <tim.k.kwak@gmail.com>
2018-01-03 11:34:47 -0500

    Move helper methods to messages_util.js

--- frontend/reducers/entities/messages_reducer.js
+++ frontend/reducers/entities/messages_reducer.js
@@ -4,6 +4,7 @@ import {
   FORCE_LOGOUT
 } from '../../actions/session_actions';
 import { RECEIVE_MESSAGES } from '../../actions/messages_actions';
+import * as MessagesUtil from '../../util/messages_util';
 
 const defaultState = {
 
@@ -13,13 +14,13 @@ const messagesReducer = (state = defaultState, action) => {
   let nextState = Object.assign({}, state);
   switch (action.type){
     case RECEIVE_MESSAGES:
-      action.payload.messages = processMessages(action.payload.messages);
+      action.payload.messages = MessagesUtil.processMessages(action.payload.messages);
       Object.keys(action.payload.messages).forEach((key) => {
         nextState[key] = action.payload.messages[key];
       });
       return nextState;
     case RECEIVE_SESSION_PAYLOAD:
-      return processMessages(action.payload.messages) ?
+      return MessagesUtil.processMessages(action.payload.messages) ?
         action.payload.messages : state;
     case RESET_STATE:
       return defaultState;
@@ -30,62 +31,5 @@ const messagesReducer = (state = defaultState, action) => {
   }
 };
 
-const processMessages = (messages) => {
-  Object.keys(messages).forEach((key) => {
-    messages[key].delta = timeDifference(messages[key].timestamp);
-  });
-  return messages;
-};
-
-const timeDifference = (time) => {
-  let then = new Date(time);
-  let now = new Date();
-  let delta = parseInt((now-then)/1000);
-  let deltaObject;
-  if (delta < 60){
-    deltaObject = letThereBeDeltaObject("s", delta, then);
-  } else {
-    delta = Math.floor(delta/60);
-    if (delta < 60){
-      deltaObject = letThereBeDeltaObject("m", delta, then);
-    } else {
-      delta = Math.floor(delta/60);
-      if (delta < 24){
-        deltaObject = letThereBeDeltaObject("h", delta, then);
-      } else {
-        delta = Math.floor(delta/24);
-        deltaObject = letThereBeDeltaObject("d", delta, then);
-      }
-    }
-  }
-  return deltaObject;
-};
-
-const letThereBeDeltaObject = (type, delta, time) => {
-  return {
-    type,
-    delta,
-    date: formatDate(time),
-    time: formatTime(time)
-  };
-};
-
-const formatDate = (time) => {
-  return time.getMonth()+1 + "/" +
-    time.getDate() + "/" +
-    (time.getYear() + 1900);
-};
-
-const formatTime = (time) => {
-  let meridiem = ' AM';
-  let hour = time.getHours();
-  if (hour > 12) {
-    hour = hour - 12;
-    meridiem = ' PM';
-  }
-  return ("0" + hour).slice(-2) + ":" +
-    ("0" + time.getMinutes()).slice(-2) + 
-    meridiem;
-};
 
 export default messagesReducer;
---
+++ frontend/util/messages_util.jsx
@@ -0,0 +1,58 @@
+
+export const processMessages = (messages) => {
+  Object.keys(messages).forEach((key) => {
+    messages[key].delta = timeDifference(messages[key].timestamp);
+  });
+  return messages;
+};
+
+export const timeDifference = (time) => {
+  let then = new Date(time);
+  let now = new Date();
+  let delta = parseInt((now-then)/1000);
+  let deltaObject;
+  if (delta < 60){
+    deltaObject = letThereBeDeltaObject("s", delta, then);
+  } else {
+    delta = Math.floor(delta/60);
+    if (delta < 60){
+      deltaObject = letThereBeDeltaObject("m", delta, then);
+    } else {
+      delta = Math.floor(delta/60);
+      if (delta < 24){
+        deltaObject = letThereBeDeltaObject("h", delta, then);
+      } else {
+        delta = Math.floor(delta/24);
+        deltaObject = letThereBeDeltaObject("d", delta, then);
+      }
+    }
+  }
+  return deltaObject;
+};
+
+export const letThereBeDeltaObject = (type, delta, time) => {
+  return {
+    type,
+    delta,
+    date: formatDate(time),
+    time: formatTime(time)
+  };
+};
+
+export const formatDate = (time) => {
+  return time.getMonth()+1 + "/" +
+    time.getDate() + "/" +
+    (time.getYear() + 1900);
+};
+
+export const formatTime = (time) => {
+  let meridiem = ' AM';
+  let hour = time.getHours();
+  if (hour > 12) {
+    hour = hour - 12;
+    meridiem = ' PM';
+  }
+  return ("0" + hour).slice(-2) + ":" +
+    ("0" + time.getMinutes()).slice(-2) +
+    meridiem;
+};
