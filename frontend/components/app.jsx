05c5ea117da1e9d94d3b3e58b3cd6bc6328a8a51
Tim Kwak <tim.k.kwak@gmail.com>
2018-04-30 19:39:27 -0400

    Refactor

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,19 +1,16 @@
 import React from 'react';
-import {Switch, Route} from 'react-router';
+import {Switch} from 'react-router';
 import {withRouter} from 'react-router-dom';
+import {connect} from 'react-redux';
 import {AuthRoute, ProtectedRoute} from '../util/route_util';
 import {toggleDropdown} from '../actions/ui_actions';
-import ModalContainer from './modal/modal_container';
 import SessionPage from './session/session_page';
 import MainPage from './main/main_page';
-import {connect} from 'react-redux';
+import ModalContainer from './modal/modal_container';
 
 const App = (props) => {
-  return (<div id='app' onClick={() => {
-      if (props.dropdownMode !== undefined) {
-        props.toggleDropdown();
-      }
-    }}>
+  return (
+  <div id='app' onClick={props.removeDropdown}>
     <Switch>
       <AuthRoute path='/login' component={SessionPage}/>
       <AuthRoute path='/signup' component={SessionPage}/>
@@ -24,12 +21,15 @@ const App = (props) => {
 };
 
 const mapStateToProps = (state, ownProps) => {
-  return {currentUser: state.session.currentUser, dropdownMode: state.ui.toggleMode};
+  return {
+    currentUser: state.session.currentUser,
+    dropdownMode: state.ui.toggleMode
+  };
 };
 
 const mapDispatchToProps = (dispatch, ownState) => {
   return {
-    toggleDropdown: (e) => dispatch(toggleDropdown(false, undefined))
+    removeDropdown: (e) => dispatch(toggleDropdown(false, undefined))
   };
 };
 
