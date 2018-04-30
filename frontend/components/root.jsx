b5a4a09f67c2bd2adbe08e7c82c0094fd27cbd2b
Tim Kwak <tim.k.kwak@gmail.com>
2018-04-30 17:23:12 -0400

    Refactor

--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import { Provider } from 'react-redux';
-import { HashRouter } from 'react-router-dom';
+import {Provider} from 'react-redux';
+import {HashRouter} from 'react-router-dom';
 import App from './app';
 
-export default ({ store }) => {
+export default({store}) => {
   return (
-    <Provider store={ store }>
+    <Provider store={store}>
       <HashRouter>
-        <App />
+        <App/>
       </HashRouter>
     </Provider>
   );
