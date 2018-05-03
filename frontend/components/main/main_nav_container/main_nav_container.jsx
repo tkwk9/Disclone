e69b1e757d0195dbb2caa916d58f2351472ffc62
Tim Kwak <tim.k.kwak@gmail.com>
2018-05-03 10:33:40 -0400

    Refactor

--- frontend/components/main/main_nav_container/main_nav_container.jsx
+++ frontend/components/main/main_nav_container/main_nav_container.jsx
@@ -1,57 +1,38 @@
 import React from 'react';
-import { withRouter, NavLink, Link } from 'react-router-dom';
-import { toggleModal } from '../../../actions/ui_actions';
-import { connect } from 'react-redux';
+import {withRouter, NavLink} from 'react-router-dom';
+import {toggleModal} from '../../../actions/ui_actions';
+import {connect} from 'react-redux';
 import DmIcon from './dm_icon';
 import ServerIcon from './server_icon';
 
-class MainNavContainer extends React.Component {
-  constructor(props){
-    super(props);
-  }
-
-  render() {
-    return (
-      <div className="main-nav">
-        <NavLink to='/@me' className='selector mp-selector' >
-          <img src={'http://res.cloudinary.com/seaside9/image/upload/v1515372379/dm-icon_qagg2j.svg'}></img>
-        </NavLink>
-        <div className='friends-online'>{`${this.props.onlineUserCount} ONLINE`}</div>
-        <ul className='dm-list'>
-          {this.props.dmList.map(
-            (dm) => {
-              return (
-                <DmIcon key={`dm: ${dm.id}`}dm={dm}/>
-              );
-            }
-          )}
-        </ul>
-        <div className='server-seperator'></div>
-        <ul className='dm-list'>
-          {this.props.serverList.map(
-            (server) => {
-              return (
-                <ServerIcon key={`server: ${server.id}`}server={server} />
-              );
-            }
-          )}
-          <div className="new-server-button" onClick={this.props.toggleAddServerModal}>+</div>
-        </ul>
-        <div className='server-seperator'></div>
-        <a className="selector social linked-in" href="https://www.linkedin.com/in/tim-kwak/"></a>
-        <a className="selector social github" href="https://github.com/Seaside9/Disclone"></a>
-        <a className="selector social website" href="http://timkwak.com"></a>
-      </div>
-    );
-  }
-}
-
+const MainNavContainer = props => {
+  return (
+    <div className="main-nav">
+      <NavLink to='/@me' className='selector mp-selector' >
+        <img src={'http://res.cloudinary.com/seaside9/image/upload/v1515372379/dm-icon_qagg2j.svg'}></img>
+      </NavLink>
+      <div className='friends-online'>{`${props.onlineUserCount} ONLINE`}</div>
+      <ul className='dm-list'>
+        {props.dmList.map(dm => (<DmIcon key={`dm: ${dm.id}`}dm={dm}/>))}
+      </ul>
+      <div className='server-seperator'></div>
+      <ul className='dm-list'>
+        {props.serverList.map(
+          server => (<ServerIcon key={`server: ${server.id}`}server={server} />)
+        )}
+        <div className="new-server-button" onClick={props.toggleAddServerModal}>+</div>
+      </ul>
+      <div className='server-seperator'></div>
+      <a className="selector social linked-in" href="https://www.linkedin.com/in/tim-kwak/"></a>
+      <a className="selector social github" href="https://github.com/Seaside9/Disclone"></a>
+      <a className="selector social website" href="http://timkwak.com"></a>
+    </div>
+  );
+};
 
 const mapStateToProps = (state, ownProps) => {
-  let dmList = [];
-  let serverList = [];
-  let friends = [];
-  let onlineUserCount = 0;
+  let dmList = [], serverList = [],
+    friends = [], onlineUserCount = 0;
   if (state.ui.sessionPayloadReceived) {
     dmList = Object.values(state.entities.directMessages).map(
       dm => {
@@ -72,19 +53,13 @@ const mapStateToProps = (state, ownProps) => {
     friends = state.session.currentUser.friendsList.map(id => {
       return state.entities.users[id];
     });
-
-    onlineUserCount = friends.filter(
-      user => {
-        return user.online;
-      }
-    ).length;
+    onlineUserCount = friends.filter(user => user.online).length;
   }
 
   return {
     dmList: dmList,
     serverList: serverList,
-    onlineUserCount: onlineUserCount,
-    currentPath: ownProps.location.pathname
+    onlineUserCount: onlineUserCount
   };
 };
 
--- frontend/components/main/main_nav_container/server_icon.jsx
+++ frontend/components/main/main_nav_container/server_icon.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { withRouter, NavLink, Link } from 'react-router-dom';
+import {withRouter, NavLink, Link} from 'react-router-dom';
 
 export default withRouter(props => {
   return (
