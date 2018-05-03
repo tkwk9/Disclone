0e973d9733d25b66296ae7d8edbda57a6176fcfa
Tim Kwak <tim.k.kwak@gmail.com>
2018-05-03 14:12:52 -0400

    Refactor ContentContainer

--- frontend/components/main/content_container/content_container.jsx
+++ frontend/components/main/content_container/content_container.jsx
@@ -6,50 +6,33 @@ import MembersList from './members_list/members_list';
 import FriendsListHead from './content_head/friends_list_head';
 import LiveChatHead from './content_head/live_chat_head';
 
-class ContentContainer extends React.Component {
-  constructor(props){
-    super(props);
-
-    this.updateFriendsList = this.updateFriendsList.bind(this);
+const ContentContainer = props => {
+  let head, content, memberList;
+  switch(props.serverId) {
+    case 'friends_list':
+      head = (<FriendsListHead />);
+      content = (<FriendsList friendsListMode={true}/>);
+      memberList = (<div></div>);
+      break;
+    case 'DM':
+      head = (<LiveChatHead type='DM' messageableId={props.messageableId} />);
+      content = (<LiveChat type='DM' messageableId={props.messageableId} />);
+      memberList = (<div></div>);
+      break;
+    default:
+      head =  (<LiveChatHead type={props.serverId} messageableId={props.messageableId} />);
+      content = (<LiveChat type={props.serverId} messageableId={props.messageableId} />);
+      memberList = (<MembersList serverId={props.serverId} />);
   }
 
-  updateFriendsList(mode) {
-    return () => {
-      this.setState({
-        head: <FriendsListHead updateFriendsList={this.updateFriendsList} friendsListMode={mode}/>,
-        content: <FriendsList friendsListMode={mode} />
-      });
-    };
-  }
-
-  render(){
-    let head, content, memberList;
-    switch(this.props.serverId) {
-      case 'friends_list':
-        head = (<FriendsListHead updateFriendsList={this.updateFriendsList} />);
-        content = (<FriendsList friendsListMode={true}/>);
-        memberList = (<div></div>);
-        break;
-      case 'DM':
-        head = (<LiveChatHead type='DM' messageableId={this.props.messageableId} />);
-        content = (<LiveChat type='DM' messageableId={this.props.messageableId} />);
-        memberList = (<div></div>);
-        break;
-      default:
-        head =  (<LiveChatHead type={this.props.serverId} messageableId={this.props.messageableId} />);
-        content = (<LiveChat type={this.props.serverId} messageableId={this.props.messageableId} />);
-        memberList = (<MembersList serverId={this.props.serverId} />);
-    }
-
-    return (
-      <div className="content-container">
-        {head}
-        {content}
-        {memberList}
-      </div>
-    );
-  }
-}
+  return (
+    <div className="content-container">
+      {head}
+      {content}
+      {memberList}
+    </div>
+  );
+};
 
 const mapStateToProps = (state, ownProps) => {
   return {
