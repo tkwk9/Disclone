3760316c9e6c2af44edce94fe62e3b820e013e68
Tim Kwak <tim.k.kwak@gmail.com>
2018-05-03 13:50:48 -0400

    Refactor FriendsList

--- frontend/components/main/content_container/friends_list/friend_selector.jsx
+++ frontend/components/main/content_container/friends_list/friend_selector.jsx
@@ -1,20 +1,45 @@
+import {connect} from 'react-redux';
+import {withRouter} from 'react-router-dom';
 import React from 'react';
+import {createDm} from '../../../../actions/direct_messages_actions';
+import {deleteFriendship} from '../../../../actions/friends_actions';
 
-export default (props) => {
+const FriendSelector = props => {
   return (
     <div className='friend-wrapper'>
-      <li className='friend-selector' onClick={props.switchDms}>
+      <li className='friend-selector' onClick={props.switchDm}>
         <div className='name-tag'>
           <div className='user-img'>
             <img src={props.friend.imgURL} />
           </div>
-          <div className='username'>{props.friend.username}<span>{props.friend.stringId}</span></div>
+          <div className='username'>
+            {props.friend.username}<span>{props.friend.stringId}</span>
+          </div>
         </div>
         <div className='status-wrapper'>
-          <div className={`status-indicator ${props.friend.online}`}></div><div className='status'>{props.friend.online ? 'Online' : 'Offline'}</div>
+          <div className={`status-indicator ${props.friend.online}`}></div>
+          <div className='status'>{props.friend.online ? 'Online' : 'Offline'}</div>
         </div>
         <div onClick={props.deleteFriendship} className="unfriend-button"></div>
       </li>
     </div>
   );
 };
+
+const mapDispatchToProps = (dispatch, ownProps) => {
+  return {
+    switchDm: () => {
+      ownProps.friend.dm
+        ? ownProps.history.push(`/@me/${ownProps.friend.dm.id}`)
+        : dispatch(createDm(ownProps.friend.id));
+      },
+    deleteFriendship: (e) => {
+      e.stopPropagation();
+      dispatch(deleteFriendship(ownProps.friend.id));
+    }
+  };
+};
+
+export default withRouter(
+  connect(null, mapDispatchToProps)(FriendSelector)
+);
--- frontend/components/main/content_container/friends_list/friends_list.jsx
+++ frontend/components/main/content_container/friends_list/friends_list.jsx
@@ -1,74 +1,34 @@
+import React from 'react';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 import FriendSelector from './friend_selector';
-import React from 'react';
-import {
-  createDm, receiveDm
-} from '../../../../actions/direct_messages_actions';
-import {
-  deleteFriendship,
-  createFriendship
-} from '../../../../actions/friends_actions';
 
-class FriendsList extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  switchDms(friend){
-    return () => {
-      if (friend.dm){
-        this.props.history.push(`/@me/${friend.dm.id}`);
-      } else {
-        this.props.createDm(friend.id);
-      }
-    };
-  }
-
-  deleteFriendship(id){
-    return (e) => {
-      e.stopPropagation();
-      this.props.deleteFriendship(id);
-    };
-  }
-
-  render() {
-    let friends = this.props.friendsList.filter(
-      friend => {
-        return friend.online || this.props.allSelected;
-      }
-    ).map((friend) => {
-      let status = friend.online ? 'green' : 'red';
-      return (
-        <FriendSelector
-          key={friend.id}
-          switchDms={this.switchDms(friend)}
-          deleteFriendship={this.deleteFriendship(friend.id)}
-          friend={friend}
-          status={status}
-        />
-      );
-    });
-    return (
-      <div className="friends-list">
-        <div className='friends-table-header'>
-          <div>NAME</div><div>STATUS</div>
-        </div>
-        <div className='scrollable'>
-          <ul>
-            {friends}
-          </ul>
-        </div>
+const FriendsList = props => {
+  return (
+    <div className="friends-list">
+      <div className='friends-table-header'>
+        <div>NAME</div><div>STATUS</div>
       </div>
-    );
-  }
-}
+      <div className='scrollable'>
+        <ul>
+          {props.friendsList
+            .filter(friend => friend.online || props.allSelected)
+            .map(friend => <FriendSelector key={friend.id} friend={friend} />)}
+        </ul>
+      </div>
+    </div>
+  );
+};
 
 const mapStateToProps = (state, ownProps) => {
-  let directMessages = Object.values(state.entities.directMessages);
-  let friendsList = state.session.currentUser.friendsList.map((id) => {
+  const directMessages = Object.values(state.entities.directMessages)
+    .reduce( (acc, dm) => {
+      acc[dm.recipientId] = dm;
+      return acc;
+    }, {});
+  const friendsList = state.session.currentUser.friendsList.map((id) => {
     let friend = state.entities.users[id];
-    friend.dm = directMessages.find((dm) => dm.recipientId === friend.id);
+    friend.dm = directMessages[friend.id];
     return friend;
   });
   return {
@@ -77,15 +37,6 @@ const mapStateToProps = (state, ownProps) => {
   };
 };
 
-const mapDispatchToProps = (dispatch, ownState) => {
-  return {
-    createDm: friendId => dispatch(createDm(friendId)),
-    receiveDm: payload => dispatch(receiveDm(payload)),
-    deleteFriendship: targetId => dispatch(deleteFriendship(targetId)),
-    createFriendship: targetId => dispatch(createFriendship(targetId))
-  };
-};
-
 export default withRouter(
-  connect(mapStateToProps, mapDispatchToProps)(FriendsList)
+  connect(mapStateToProps, null)(FriendsList)
 );
