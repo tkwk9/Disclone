9ba5d41580d612bee3b858f48e7104876a1e78ef
Tim Kwak <tim.k.kwak@gmail.com>
2018-05-07 16:43:17 -0400

    Change class names

--- frontend/components/main/main_nav_container/server_icon.jsx
+++ frontend/components/main/main_nav_container/server_icon.jsx
@@ -3,13 +3,11 @@ import {withRouter, NavLink, Link} from 'react-router-dom';
 
 export default withRouter(props => {
   return (
-    <div key={props.server.id} className={`selector-wrapper true`} >
-      <NavLink className={`selector s-selector true`}  to={`/${props.server.id}`}>
+      <NavLink className={`selector s-selector`}  to={`/${props.server.id}`}>
         <div className='server-img'>
           {props.server.imgURL ? <img src={props.server.imgURL}></img> : props.server.name.match(/\b(\w)/g).join('').slice(0,2)}
           {props.server.unreadCount ? <div className={`unreadCounter`}>{props.server.unreadCount}</div> : <div></div>}
         </div>
       </NavLink>
-    </div>
   );
 });
