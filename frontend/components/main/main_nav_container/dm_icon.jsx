b18ca6c532f88ea28f5473b255ad2c4751c07cd2
Tim Kwak <tim.k.kwak@gmail.com>
2018-05-07 21:35:35 -0400

    Fix animation bug

--- frontend/components/main/main_nav_container/dm_icon.jsx
+++ frontend/components/main/main_nav_container/dm_icon.jsx
@@ -1,17 +1,32 @@
 import React from 'react';
 import {withRouter, Link} from 'react-router-dom';
 
-export default withRouter(props => {
-  return (
-      <Link className={`selector dm-selector ${props.dm.unreadCount > 0}`}  to={`/@me/${props.dm.id}`}>
-        <div className={`dm-icon-wrapper ${props.dm.unreadCount > 0}`}>
-          <div className='user-img'>
-            <div className='image-holder'>
-              <img src={props.dm.recipient.imgURL}></img>
+class DmIcon extends React.Component {
+  constructor(props) {
+    super(props);
+    this.dmSelectorClass = this.props.dm.unreadCount > 0 || 'new';
+  }
+
+  componentWillReceiveProps(newProps) {
+    this.dmSelectorClass = this.dmSelectorClass === 'new'
+      ? newProps.dm.unreadCount > 0 ? true : 'new'
+      : newProps.dm.unreadCount > 0;
+  }
+
+  render() {
+    return (
+        <Link className={`selector dm-selector ${this.dmSelectorClass}`}  to={`/@me/${this.props.dm.id}`}>
+          <div className={`dm-icon-wrapper ${this.dmSelectorClass}`}>
+            <div className='user-img'>
+              <div className='image-holder'>
+                <img src={this.props.dm.recipient.imgURL}></img>
+              </div>
+              <div className={`unreadCounter`}>{this.props.dm.unreadCount}</div>
             </div>
-            <div className={`unreadCounter`}>{props.dm.unreadCount}</div>
           </div>
-        </div>
-      </Link>
-  );
-});
+        </Link>
+    );
+  }
+}
+
+export default withRouter(DmIcon);
